897c3b35d4fee71a137e1136ca8dd1fe0a77ea03
Rabbani <thinkoverthink@gmail.com>
2022-02-10 21:42:48 +0530

    first node app

---
+++ app.js
@@ -0,0 +1,9 @@
+const name2 = "Quasar Rabbani2"
+const amount = 12
+if (amount<12) {
+    console.log("small")   
+} else {
+    console.log("big")
+}
+console.log("this is my first app")
+console.log(name2)
